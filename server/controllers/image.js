ee408b4f0b320c6188832bb4d9eff8471fb8506d
honzik-erbi <jan.erben@student.spsmb.cz>
2024-04-08 10:19:26 +0100

    jj

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -5,6 +5,9 @@ const multer = require("multer")
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        
+    cb(null, path.join(__dirname, "../public/img"))
+    },
+    filename: (req, file, cb) => {
+        cb(null, )
     }
 })
